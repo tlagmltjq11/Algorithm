e4f18285cb5ec082d63a21a1d6bf4ce97b95e43b
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2020-11-21 19:32:51 +0900

    4673_셀프넘버_201121

---
+++ 4673_셀프넘버.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CodingTestPractice
+{
+    //4673 셀프넘버
+    class Program
+    {
+        static int D(int n)
+        {
+            int result = 0;
+
+            while (n != 0)
+            {
+                result += n % 10;
+                n /= 10;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] data = new int[10001];
+
+            for (int i = 1; i < 10000; i++)
+            {
+                int cNum = D(i) + i;
+
+                if (cNum <= 10000)
+                {
+                    data[cNum] = 1;
+                }
+            }
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (data[i] == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
