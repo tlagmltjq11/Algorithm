584ec6846bb2fbccc533bf9975625b9ef5529da0
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2020-11-21 20:43:36 +0900

    Create 1065.cs

---
+++ 1065.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _1065
+    {
+        static int han(int n)
+        {
+            int cnt = 0;
+
+            if(n <= 99)
+            {
+                return n;
+            }
+            else
+            {
+                int a, b, c;
+                for(int i=100; i<=n; i++)
+                {
+                    a = i / 100;
+                    b = (i / 10) % 10;
+                    c = i % 10;
+
+                    if((c-b) == (b-a))
+                    {
+                        cnt++;
+                    }
+                }
+
+                cnt += 99;
+            }
+
+            return cnt;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write(han(n));
+        }
+    }
+}
