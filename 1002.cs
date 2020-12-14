c6fb5534404573f63c7a457640b7831bc24e78b4
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2020-12-14 19:04:21 +0900

    20201214

---
+++ 1002.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _1002
+    {
+        static void Main()
+        {
+            int testCase = int.Parse(Console.ReadLine());
+            int x1, y1, r1, x2, y2, r2;
+
+            for(int i=0; i<testCase; i++)
+            {
+                string[] data = Console.ReadLine().Split(" ");
+
+                x1 = int.Parse(data[0]);
+                y1 = int.Parse(data[1]);
+                r1 = int.Parse(data[2]);
+
+                x2 = int.Parse(data[3]);
+                y2 = int.Parse(data[4]);
+                r2 = int.Parse(data[5]);
+
+                int distance = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+                int radSum = (r1 + r2) * (r1 + r2);
+                int radDif = (r2 - r1) * (r2 - r1);
+
+                //좌표가 같은경우
+                if (distance == 0)
+                {
+                    //반지름까지 같은경우
+                    if (r1 == r2)
+                    {
+                        Console.WriteLine(-1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(0);
+                    }
+                }
+                //두점에서 원이 만나는 경우
+                else if (radDif < distance && distance < radSum)
+                {
+                    Console.WriteLine(2);
+                }
+                //외접 혹은 내접하는 경우
+                else if (radSum == distance || radDif == distance)
+                {
+                    Console.WriteLine(1);
+                }
+                //아예 멀리 떨어져 만나지못하는 경우
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
---
+++ 3009.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _3009
+    {
+        static void Main()
+        {
+            string[] x = new string[3];
+            string[] y = new string[3];
+
+            for(int i=0; i<3; i++)
+            {
+                string[] data = Console.ReadLine().Split(" ");
+
+                x[i] = data[0];
+                y[i] = data[1];
+            }
+
+            if(x[0] == x[1])
+            {
+                Console.Write(x[2]);
+            }
+            else if(x[0] == x[2])
+            {
+                Console.Write(x[1]);
+            }
+            else
+            {
+                Console.Write(x[0]);
+            }
+
+            Console.Write(" ");
+
+            if (y[0] == y[1])
+            {
+                Console.Write(y[2]);
+            }
+            else if (y[0] == y[2])
+            {
+                Console.Write(y[1]);
+            }
+            else
+            {
+                Console.Write(y[0]);
+            }
+        }
+    }
+}
