06d85cb605548c124884ea72d37806c92c45dba1
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2020-12-15 19:05:53 +0900

    20201215

---
+++ 10870.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _10870
+    {
+        static int Fibo(int n)
+        {
+            if(n == 0)
+            {
+                return 0;
+            }
+            else if(n <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibo(n - 1) + Fibo(n - 2);
+            }
+        }
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Fibo(n));
+        }
+    }
+}
---
+++ 10872.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _10872
+    {
+        /*
+        static int Facto(int n)
+        {
+            if(n > 1)
+            {
+                return n * Facto(n - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Facto(N));
+        }
+        */
+    }
+}
