1dd5ab66c56e8c79ffc7f779f0f1baac131f8141
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-01-03 17:44:29 +0900

    20210103

---
+++ 2231.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _2231
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int temp = N;
+            int digit = 0;
+            bool check = false;
+
+            while(temp > 0)
+            {
+                temp /= 10;
+                digit++;
+            }
+
+            //생성자 + 각 자릿수인데 각 자릿수마다 최대값은 9임
+            //고로 자릿수 * 9 만큼만 반복해보면 시간을 단축시킬 수 있다.
+            for (int i=N - (digit * 9); i<N; i++)
+            {
+                int sum = i;
+                int _digit = i;
+
+                while(_digit > 0)
+                {
+                    sum += _digit % 10;
+                    _digit /= 10;
+                }
+
+                if(sum == N)
+                {
+                    Console.WriteLine(i);
+                    check = true;
+                    break;
+                }
+            }
+
+            if (!check)
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
---
+++ 7568.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    //슬슬 c언어로 갈아타야할듯..
+    class _7568
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<KeyValuePair<int, int>> m_list = new List<KeyValuePair<int, int>>();
+            int[] m_compare = new int[N];
+
+            for(int i=0; i<N; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(" ");
+                m_list.Add(new KeyValuePair<int, int>(int.Parse(inputs[0]), int.Parse(inputs[1])));
+            }
+
+            for(int i=0; i<N; i++)
+            {
+                var kvp = m_list[i];
+
+                for(int j=i+1; j<N; j++)
+                {
+                    //i가 덩치가 큰 경우
+                    if(kvp.Key > m_list[j].Key && kvp.Value > m_list[j].Value)
+                    {
+                        m_compare[j]++;
+                    }
+                    //j가 덩치가 큰 경우
+                    else if(kvp.Key < m_list[j].Key && kvp.Value < m_list[j].Value)
+                    {
+                        m_compare[i]++;
+                    }
+                }
+            }
+
+            for(int i=0; i<N; i++)
+            {
+                Console.Write(m_compare[i] + 1);
+                Console.Write(" ");
+            }
+        }
+    }
+}
