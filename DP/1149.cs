cdb9d4197e5dc6c00980d3113ea1561798db1829
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-02-12 21:57:46 +0900

    DP

---
+++ DP/1149.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _1149
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int R, G, B, r, g, b;
+            r = g = b = 0;
+
+            for (int i = 0; i < N; ++i)
+            {
+                string[] inputs = Console.ReadLine().Split();
+                R = int.Parse(inputs[0]);
+                G = int.Parse(inputs[1]);
+                B = int.Parse(inputs[2]);
+
+                //현재까지 진행된 값을 계속해서 누적하는것이 포인트
+                R += Math.Min(g, b);
+                G += Math.Min(r, b);
+                B += Math.Min(r, g);
+
+                r = R;
+                g = G;
+                b = B;
+            }
+            Console.Write(Math.Min(Math.Min(r, g), b));
+        }
+    }
+}
---
+++ DP/9461.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    /*
+    class _9461
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int N = int.Parse(Console.ReadLine());
+            long[] nums = new long[101];
+
+            nums[1] = nums[2] = nums[3] = 1;
+            nums[4] = nums[5] = 2;
+
+            for(int i=6; i<101; i++)
+            {
+                //파도반 수열의 규칙은 N번째 수 = N-1번째 수 + N-5번째 수
+                nums[i] = nums[i - 1] + nums[i - 5];
+            }
+
+            for(int i=0; i<N; i++)
+            {
+                int read = int.Parse(Console.ReadLine());
+                sb.AppendLine(nums[read].ToString());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+    */
+}
