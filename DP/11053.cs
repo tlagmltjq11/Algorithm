1e702ade5027958b9d9786fdb3e8b3aef837c45d
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-02-16 18:46:22 +0900

    DP

---
+++ DP/11053.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _11053
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), s => int.Parse(s));
+            int[] dp = new int[n];
+
+            dp[0] = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int max = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    //현재 값보다 큰 경우는 증가수열에 포함되지 않으므로 검사할 필요가 없다.
+                    if (nums[i] > nums[j])
+                    {
+                        max = Math.Max(max, dp[j]); //제일 긴 수열값을 찾는다.
+                    }
+                }
+                dp[i] = max + 1; //제일 긴 수열에 자신을 포함시켜 저장한다.
+            }
+
+            int result = -1;
+            for (int i = 0; i < n; i++)
+            {
+                result = Math.Max(dp[i], result);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ DP/2156.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _2156
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] wine = new int[n+1];
+            int[] dp = new int[10001];
+
+            for(int i=1; i<=n; i++)
+            {
+                wine[i] = int.Parse(Console.ReadLine());
+            }
+
+            dp[0] = 0;
+            dp[1] = wine[1];
+
+            if (n > 1)
+            {
+                dp[2] = dp[1] + wine[2];
+            }
+
+            if (n > 2)
+            {
+                for (int i = 3; i <= n; i++)
+                {
+                    dp[i] = Math.Max(dp[i - 3] + wine[i - 1] + wine[i], Math.Max(dp[i - 1], dp[i - 2] + wine[i]));
+                }
+            }
+
+            Console.WriteLine(dp[n]);
+
+        }
+    }
+}
