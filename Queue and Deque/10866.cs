8cb10cab1d80ea9b860218a7e8a4a53f7aba2dea
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-03-05 02:21:29 +0900

    덱 문제

---
+++ Queue and Deque/10866.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    /*
+    class Node
+    {
+        public int data;
+        public Node link;
+    }
+
+    class Deque
+    {
+        public Node header, tail;
+        public int curSize;
+
+        public void Init()
+        {
+            header = new Node();
+            tail = new Node();
+            curSize = 0;
+        }
+
+
+        public void push_Front(int data)
+        {
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if(isEmpty() == 1)
+            {
+                header.link = newNode;
+                tail.link = newNode;
+                curSize++;
+
+                return;
+            }
+            else
+            {
+                newNode.link = header.link;
+                header.link = newNode;
+                curSize++;
+
+                return;
+            }
+        }
+
+        public void push_Back(int data)
+        {
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if (isEmpty() == 1)
+            {
+                header.link = newNode;
+                tail.link = newNode;
+                curSize++;
+
+                return;
+            }
+            else
+            {
+                tail.link.link = newNode;
+                tail.link = newNode;
+                curSize++;
+
+                return;
+            }
+        }
+
+        public int pop_Front()
+        {
+            if(isEmpty() == 0)
+            {
+                Node delNode = header.link;
+                header.link = delNode.link;
+
+                curSize--;
+
+                return delNode.data;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int pop_Back()
+        {
+            if (isEmpty() == 0)
+            {
+                Node delNode = tail.link;
+
+                Node iter = header.link;
+                for(int i=0; i<curSize-2; i++)
+                {
+                    iter = iter.link;
+                }
+
+                tail.link = iter;
+
+                curSize--;
+
+                return delNode.data;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int Size()
+        {
+            return curSize;
+        }
+
+        public int isEmpty()
+        {
+            if(curSize == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Front()
+        {
+            if(isEmpty() == 1)
+            {
+                return -1;
+            }
+            else
+            {
+                return header.link.data;
+            }
+        }
+
+        public int Back()
+        {
+            if (isEmpty() == 1)
+            {
+                return -1;
+            }
+            else
+            {
+                return tail.link.data;
+            }
+        }
+    }
+    
+    class _10866
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            Deque dq = new Deque();
+            dq.Init();
+
+            for(int i=0; i<n; i++)
+            {
+                string[] read = Console.ReadLine().Split(" ");
+
+                switch(read[0])
+                {
+                    case "push_front":
+                        dq.push_Front(int.Parse(read[1]));
+                        break;
+                    case "push_back":
+                        dq.push_Back(int.Parse(read[1]));
+                        break;
+                    case "pop_front":
+                        sb.AppendLine(dq.pop_Front().ToString());
+                        break;
+                    case "pop_back":
+                        sb.AppendLine(dq.pop_Back().ToString());
+                        break;
+                    case "size":
+                        sb.AppendLine(dq.Size().ToString());
+                        break;
+                    case "empty":
+                        sb.AppendLine(dq.isEmpty().ToString());
+                        break;
+                    case "front":
+                        sb.AppendLine(dq.Front().ToString());
+                        break;
+                    case "back":
+                        sb.AppendLine(dq.Back().ToString());
+                        break;
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+    */
+}
---
+++ Queue and Deque/5430.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodingTestPractice
+{
+    class _5430
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                string func = Console.ReadLine();
+                int size = int.Parse(Console.ReadLine());
+
+                if (size == 0)
+                {
+                    sb.AppendLine("error");
+                    continue;
+                }
+
+                string nums = Console.ReadLine();
+                List<string> list;
+
+                nums = nums.Replace("[", "");
+                nums = nums.Replace("]", "");
+
+                string[] temp = nums.Split(",");
+                list = temp.ToList<string>();
+
+                func = func.Replace("RR", "");
+
+                int cur = 0;
+                int rCnt = 0;
+                bool check = false;
+                for (int j = 0; j < func.Length; j++)
+                {
+                    if (func[j] == 'R')
+                    {
+                        rCnt++;
+
+                        if (cur == 0)
+                        {
+                            cur = list.Count - 1;
+                        }
+                        else
+                        {
+                            cur = 0;
+                        }
+                    }
+                    else if (func[j] == 'D')
+                    {
+                        if (list.Count == 0)
+                        {
+                            sb.AppendLine("error");
+                            check = true;
+                            break;
+                        }
+                        else
+                        {
+                            if (cur != 0)
+                            {
+                                cur = list.Count - 1;
+                            }
+
+                            list.RemoveAt(cur);
+                        }
+                    }
+                }
+
+                if (check == false)
+                {
+                    sb.Append("[");
+                    if (rCnt % 2 == 0)
+                    {
+                        for (int j = 0; j < list.Count; j++)
+                        {
+                            sb.Append(list[j]);
+
+                            if (j != list.Count - 1)
+                            {
+                                sb.Append(",");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int j = list.Count - 1; j >= 0; j--)
+                        {
+                            sb.Append(list[j]);
+
+                            if (j != 0)
+                            {
+                                sb.Append(",");
+                            }
+                        }
+                    }
+                    sb.AppendLine("]");
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
