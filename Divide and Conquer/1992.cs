6bfec64b4b21883194d6ff06bdf153b5c5881450
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-03-15 21:06:57 +0900

    분할정복

---
+++ Divide and Conquer/1992.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    /*
+    class _1992
+    {
+        static int[,] arr;
+        static StringBuilder sb = new StringBuilder();
+
+        static void Divide(int n, int r, int c)
+        {
+            for(int i=r; i<n+r; i++)
+            {
+                for(int j=c; j<n+c; j++)
+                {
+                    if(arr[r,c] != arr[i,j])
+                    {
+                        sb.Append("(");
+                        Divide(n / 2, r, c);
+                        Divide(n / 2, r, c + n / 2);
+                        Divide(n / 2, r + n / 2, c);
+                        Divide(n / 2, r + n / 2, c + n / 2);
+                        sb.Append(")");
+
+                        return;
+                    }
+                }
+            }
+
+            if (arr[r, c].Equals('0'))
+            {
+                sb.Append("0");
+            }
+            else
+            {
+                sb.Append("1");
+            }
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            arr = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string read = Console.ReadLine();
+
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(read[j]);
+                }
+            }
+
+            Divide(n, 0, 0);
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+    */
+}
---
+++ Divide and Conquer/2630.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    /*
+    class _2630
+    {
+        static int w, b;
+        static int[,] arr; 
+
+        static void Divide(int n, int r, int c)
+        {
+            int temp = arr[r, c];
+
+            for(int i=r; i<n + r; i++) // + r을 해줘야 r값부터 시작해 빨리 끝나는 것을 방지
+            {
+                for(int j=c; j<n + c; j++)
+                {
+                    if(arr[i, j] != temp)
+                    {
+                        Divide(n / 2, r, c);
+                        Divide(n / 2, r, c + n / 2);
+                        Divide(n / 2, r + n / 2, c);
+                        Divide(n / 2, r + n / 2, c + n / 2);
+
+                        return; //나누어진 사각형은 갯수 덧셈 방지.
+                    }
+                }
+            }
+
+            //전부 순회했을때 return되지 않은 즉 색이 전부 같은 경우
+            if(temp == 0)
+            {
+                w++;
+            }
+            else
+            {
+                b++;
+            }
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            arr = new int[n, n];
+
+            for(int i=0; i<n; i++)
+            {
+                string[] read = Console.ReadLine().Split(" ");
+
+                for(int j=0; j<n; j++)
+                {
+                    arr[i, j] = int.Parse(read[j]);
+                }
+            }
+
+            Divide(n, 0, 0);
+
+            Console.WriteLine(w);
+            Console.WriteLine(b);
+        }
+    }
+    */
+}
