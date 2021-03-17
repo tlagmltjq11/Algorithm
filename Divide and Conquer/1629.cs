ec3c20368bb5719e98ce6dc92971772be736e951
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-03-17 18:00:56 +0900

    분할정복

---
+++ Divide and Conquer/1629.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    /*
+    class _1629
+    {
+        static long Divide(long a, long b, long c)
+        {
+            if(b == 0)
+            {
+                return 1;
+            }
+
+            long n = Divide(a, b / 2, c);
+            long temp = n * n % c;
+
+            if(b % 2 == 0)
+            {
+                return temp;
+            }
+            else
+            {
+                return a * temp % c;
+            }
+        }
+
+        static void Main()
+        {
+            long a, b, c;
+            string[] read = Console.ReadLine().Split(" ");
+
+            a = int.Parse(read[0]);
+            b = int.Parse(read[1]);
+            c = int.Parse(read[2]);
+
+            long result = Divide(a, b, c);
+
+            Console.WriteLine(result);
+        }
+    }
+    */
+}
---
+++ Divide and Conquer/2740.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    /*
+    class _2740
+    {
+        static int[,] Multi(int[,] a, int[,] b)
+        {
+            int[,] result = new int[a.GetLength(0), b.GetLength(1)];
+
+            for(int i=0; i<a.GetLength(0); i++)
+            {
+                for(int j=0; j<b.GetLength(1); j++)
+                {
+                    int temp = 0;
+
+                    for(int k=0; k<a.GetLength(1); k++)
+                    {
+                        temp += a[i, k] * b[k, j];
+                    }
+
+                    result[i, j] = temp;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            string[] read;
+            int ar, ac;
+            int br, bc;
+            int[,] a, b, c;
+
+            read = Console.ReadLine().Split(" ");
+            ar = int.Parse(read[0]);
+            ac = int.Parse(read[1]);
+
+            a = new int[ar, ac];
+            for(int i=0; i<ar; i++)
+            {
+                read = Console.ReadLine().Split(" ");
+                for(int j=0; j<ac; j++)
+                {
+                    a[i, j] = int.Parse(read[j]);
+                }
+            }
+
+            read = Console.ReadLine().Split(" ");
+            br = int.Parse(read[0]);
+            bc = int.Parse(read[1]);
+
+            b = new int[br, bc];
+            for (int i = 0; i < br; i++)
+            {
+                read = Console.ReadLine().Split(" ");
+                for (int j = 0; j < bc; j++)
+                {
+                    b[i, j] = int.Parse(read[j]);
+                }
+            }
+
+            c = Multi(a, b);
+
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<c.GetLength(0); i++)
+            {
+                for(int j=0; j<c.GetLength(1); j++)
+                {
+                    sb.Append(c[i, j].ToString() + " ");
+                }
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+    */
+}
