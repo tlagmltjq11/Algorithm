cb831fe2e28868556ae42c3cc269410106fb78ff
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-03-19 17:48:12 +0900

    분할정복 and 이진탐색

---
+++ BinarySearch/1920.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _1920
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), s => int.Parse(s));
+            Array.Sort(nums);
+
+            int m = int.Parse(Console.ReadLine());
+            int[] test = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), s => int.Parse(s));
+
+            for(int i=0; i<test.Length; i++)
+            {
+                int left = 0;
+                int right = nums.Length - 1;
+                int find = test[i];
+                bool check = false;
+
+                while(left <= right)
+                {
+                    int index = (left + right) / 2;
+                    int mid = nums[index];
+                    
+                    if(mid == find)
+                    {
+                        check = true;
+                        sb.AppendLine("1");
+                        break;
+                    }
+                    else if(mid > find)
+                    {
+                        right = index - 1;
+                    }
+                    else
+                    {
+                        left = index + 1;
+                    }
+                }
+
+                if(!check)
+                {
+                    sb.AppendLine("0");
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ Divide and Conquer/11444.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _11444
+    {
+        static long[,] Multi(long[,] a, long[,] b)
+        {
+            long[,] result = new long[a.GetLength(0), b.GetLength(1)];
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    long temp = 0;
+
+                    for (int k = 0; k < a.GetLength(1); k++)
+                    {
+                        temp += a[i, k] * b[k, j];
+                    }
+
+                    result[i, j] = temp % 1000000007;
+                }
+            }
+
+            return result;
+        }
+
+        static int Main()
+        {
+            long n = int.Parse(Console.ReadLine());
+
+            if(n<=1)
+            {
+                Console.WriteLine(n);
+                return 0;
+            }
+
+            long[,] ans = { { 1, 0 }, { 0, 1 } };
+            long[,] a = { { 1, 1 }, { 1, 0 } };
+
+            while(n > 0)
+            {
+                if(n%2 == 1)
+                {
+                    ans = Multi(ans, a);
+                }
+
+                a = Multi(a, a);
+                n /= 2;
+            }
+
+            Console.WriteLine(ans[0, 1]);
+
+            return 0;
+        }
+    }
+}
