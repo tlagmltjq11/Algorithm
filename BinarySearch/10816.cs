5391890315eed77c040e34dc4c4fe807aabbe680
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-03-23 17:56:40 +0900

    이진탐색

---
+++ BinarySearch/10816.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _10816
+    {
+        static int[] ns;
+
+        static int lower_bound(int k)
+        {
+            int left = 0;
+            int right = ns.Length - 1;
+            int mid;
+
+            while(left < right)
+            {
+                mid = (left + right) / 2;
+
+                if(ns[mid] >= k)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return right;
+        }
+
+        static int upper_bound(int k)
+        {
+            int left = 0;
+            int right = ns.Length - 1;
+            int mid;
+
+            while (left < right)
+            {
+                mid = (left + right) / 2;
+
+                if (ns[mid] > k)
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            if(ns[right] == k)
+            {
+                right++;
+            }
+
+            return right;
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            ns = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), s => int.Parse(s));
+
+            int m = int.Parse(Console.ReadLine());
+            int[] ms = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), s => int.Parse(s));
+
+            Array.Sort(ns);
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=0; i<m; i++)
+            {
+                int k = ms[i];
+                int low = lower_bound(k);
+                int up = upper_bound(k);
+
+                sb.Append((up - low).ToString() + " ");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ BinarySearch/10816_test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _10816_test
+    {
+        static int[] ns;
+        static int cnt = 0;
+
+        static void BS(int left, int right, int key)
+        {
+            if (left > right)
+            {
+                return;
+            }
+
+            int mid = (left + right) / 2;
+
+            if(ns[mid] == key)
+            {
+                cnt++;
+
+                BS(left, mid - 1, key);
+                BS(mid + 1, right, key);
+            }
+            else if(ns[mid] > key)
+            {
+                BS(left, mid - 1, key);
+            }
+            else
+            {
+                BS(mid + 1, right, key);
+            }
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            ns = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), s => int.Parse(s));
+
+            int m = int.Parse(Console.ReadLine());
+            int[] ms = Array.ConvertAll<string, int>(Console.ReadLine().Split(" "), s => int.Parse(s));
+
+            Array.Sort(ns);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < m; i++)
+            {
+                int k = ms[i];
+                BS(0, n - 1, k);
+                sb.Append(cnt.ToString() + " ");
+
+                cnt = 0;
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
