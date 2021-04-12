977d5ae5b84e30d0747e4fc78eeb6b8224d483c0
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-04-12 20:24:50 +0900

    Graph-최단경로

---
+++ Graph-ShortestPath/11657.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+	class Graph
+	{
+		int n; //정점의 개수
+		long[] distance;
+		int maxSize; //최대 정점 개수
+		public List<KeyValuePair<long, int>>[] adj_Link; //각 정점의 인접정점들을 저장해둔 리스트들의 배열
+
+		public Graph(int maxVertex)
+		{
+			n = 0;
+			maxSize = maxVertex;
+
+			distance = new long[maxSize];
+			adj_Link = new List<KeyValuePair<long, int>>[maxSize];
+		}
+
+		public void Init()
+		{
+			for (int i = 0; i < maxSize; i++)
+			{
+				distance[i] = int.MaxValue;
+			}
+		}
+
+		public void insert_Vertex(int vertex)
+		{
+			if ((n + 1) > maxSize)
+			{
+				//정점개수 초과 오류
+				return;
+			}
+
+			adj_Link[n] = new List<KeyValuePair<long, int>>();
+			n++;
+		}
+
+		public void insert_Edge(int start, int end, long weight)
+		{
+			if (start > n || end > n)
+			{
+				//정점 번호 오류
+				return;
+			}
+
+			adj_Link[start - 1].Add(new KeyValuePair<long, int>(weight, end - 1));
+		}
+
+		public void BellmanFord(int start)
+        {
+			for (int i = 0; i < n; i++)
+			{
+				distance[i] = int.MaxValue; //초기화
+			}
+			distance[start-1] = 0;
+
+			for (int i = 0; i < n; i++) //정점 갯수 만큼
+			{
+				for (int j = 0; j < n; j++) //이하 2중 루프는 모든 간선을 인접리스트 방식으로 순회하기 위함.
+				{
+					for(int k=0; k<adj_Link[j].Count; k++)
+					{
+						if (distance[j] == int.MaxValue)
+						{
+							//시작점이 무한대인 경우는 어차피 결과가 무한대이므로 넘어감.
+							continue;
+						}
+
+						distance[adj_Link[j][k].Value] = Math.Min(distance[adj_Link[j][k].Value], distance[j] + adj_Link[j][k].Key);
+					}
+				}
+			}
+
+			//음의 사이클이 존재하는지 확인하기 위해 한번의 사이클을 더 돌려본다.
+			for (int j = 0; j < n; j++)
+			{
+				for (int k = 0; k < adj_Link[j].Count; k++)
+				{
+					if (distance[j] == int.MaxValue)
+					{
+						//시작점이 무한대인 경우는 어차피 결과가 무한대이므로 넘어감.
+						continue;
+					}
+
+					if(distance[adj_Link[j][k].Value] > distance[j] + adj_Link[j][k].Key)
+                    {
+						Console.WriteLine(-1);
+						return;
+                    }
+				}
+			}
+
+			//음의 사이클이 없다면
+			StringBuilder sb = new StringBuilder();
+			for(int i=1; i<n; i++)
+            {
+				if(distance[i] == int.MaxValue)
+                {
+					sb.AppendLine("-1");
+				}
+				else
+                {
+					sb.AppendLine(distance[i].ToString());
+				}
+            }
+
+			Console.Write(sb.ToString());
+		}
+	}
+
+	class _11657
+    {
+        static void Main()
+        {
+			string[] read = Console.ReadLine().Split(" ");
+			int n = int.Parse(read[0]);
+			int m = int.Parse(read[1]);
+
+			Graph g = new Graph(n);
+			g.Init();
+
+			for(int i=0; i<n; i++)
+            {
+				g.insert_Vertex(i);
+            }
+
+			for(int i=0; i<m; i++)
+            {
+				read = Console.ReadLine().Split(" ");
+
+				g.insert_Edge(int.Parse(read[0]), int.Parse(read[1]), long.Parse(read[2]));
+            }
+
+			g.BellmanFord(1);
+        }
+    }
+}
---
+++ Graph-ShortestPath/1504.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+	class Graph
+	{
+		int n; //정점의 개수
+		bool[] visited; //방문했는지 체크
+		int[] distance; //다익스트라에서 사용될 최단거리 배열
+		int maxSize; //최대 정점 개수
+		public List<KeyValuePair<int, int>>[] adj_Link; //각 정점의 인접정점들을 저장해둔 리스트들의 배열
+
+		public Graph(int maxVertex)
+		{
+			n = 0;
+			maxSize = maxVertex;
+
+			visited = new bool[maxSize];
+			distance = new int[maxSize];
+			adj_Link = new List<KeyValuePair<int, int>>[maxSize];
+		}
+
+		public void Init()
+		{
+			for (int i = 0; i < maxSize; i++)
+			{
+				visited[i] = false;
+				distance[i] = int.MaxValue;
+			}
+		}
+
+		public void insert_Vertex(int vertex)
+		{
+			if ((n + 1) > maxSize)
+			{
+				//정점개수 초과 오류
+				return;
+			}
+			adj_Link[n] = new List<KeyValuePair<int, int>>();
+			n++;
+		}
+
+		public void insert_Edge(int start, int end, int weight)
+		{
+			if (start > n || end > n)
+			{
+				//정점 번호 오류
+				return;
+			}
+
+			adj_Link[start - 1].Add(new KeyValuePair<int, int>(weight, end - 1));
+			adj_Link[end - 1].Add(new KeyValuePair<int, int>(weight, start - 1));
+		}
+
+		public int Dijkstra(int start, int end)
+		{
+			for (int i = 0; i < adj_Link[start-1].Count; i++)
+			{
+				distance[adj_Link[start-1][i].Value] = adj_Link[start - 1][i].Key;
+			}
+
+			distance[start-1] = 0;
+			visited[start-1] = true;
+
+			//다익스트라는 노드가 n개일때 n-1번이면 모든 최단거리를 구할 수 있음.
+			for (int i = 0; i < n - 1; i++)
+			{
+				int min = int.MaxValue;
+				int min_index = -1;
+
+				//방문하지 않은 노드 중 최소값을 찾는다.
+				for (int j = 0; j < maxSize; j++)
+				{
+					if (visited[j] == false && distance[j] < min)
+					{
+						min = distance[j];
+						min_index = j;
+					}
+				}
+
+				if (min_index == -1)
+				{
+					break;
+				}
+
+				visited[min_index] = true;
+				for (int j = 0; j < adj_Link[min_index].Count; j++)
+				{
+					if (visited[adj_Link[min_index][j].Value] == false)
+					{
+						if (distance[adj_Link[min_index][j].Value] > adj_Link[min_index][j].Key + distance[min_index])
+						{
+							distance[adj_Link[min_index][j].Value] = adj_Link[min_index][j].Key + distance[min_index];
+						}
+					}
+				}
+			}
+
+			return distance[end-1];
+		}
+	}
+
+	class _1504
+    {
+        static void Main()
+        {
+            string[] read = Console.ReadLine().Split(" ");
+
+            int n = int.Parse(read[0]);
+            int e = int.Parse(read[1]);
+
+			Graph g = new Graph(n);
+			g.Init();
+
+			for(int i=0; i<n; i++)
+            {
+				g.insert_Vertex(i);
+            }
+
+            for(int i=0; i<e; i++)
+            {
+                read = Console.ReadLine().Split(" ");
+                int u = int.Parse(read[0]);
+                int v = int.Parse(read[1]);
+                int w = int.Parse(read[2]);
+
+				g.insert_Edge(u, v, w);
+            }
+
+			read = Console.ReadLine().Split(" ");
+			int n1 = int.Parse(read[0]);
+			int n2 = int.Parse(read[1]);
+
+			// 1 -> n1 -> n2 -> n
+			int temp1 = 0;
+			temp1 += g.Dijkstra(1, n1);
+			temp1 += g.Dijkstra(n1, n2);
+			temp1 += g.Dijkstra(n2, n);
+
+			// 1 -> n2 -> n1 -> n
+			int temp2 = 0;
+			temp2 += g.Dijkstra(1, n2);
+			temp2 += g.Dijkstra(n2, n1);
+			temp2 += g.Dijkstra(n1, n);
+
+			int result = Math.Min(temp1, temp2);
+
+			if(result == int.MaxValue)
+            {
+				Console.WriteLine(-1);
+            }
+			else
+            {
+				Console.WriteLine(result);
+            }
+		}
+    }
+}
