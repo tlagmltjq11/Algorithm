4dd621f4fae6bf79d9fc2130f76879e354bc35b6
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-04-28 20:52:15 +0900

    프로그래머스

---
+++ 프로그래머스_Lv2/기능개발.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 기능개발
+    {
+
+        static int[] solution(int[] progresses, int[] speeds)
+        {
+            int[] answer = new int[100];
+
+            //남은 작업일 수를 저장할 큐
+            Queue<int> days = new Queue<int>();
+
+            //각 작업마다 남은 작업 일 수를 스피드로 나누어 구한다.
+            for(int i=0; i<progresses.Length; i++)
+            {
+                double temp = 100 - progresses[i];
+                temp = temp / speeds[i];
+
+                if(temp - (int)temp > 0)
+                {
+                    temp++;
+                    days.Enqueue((int)temp);
+                }
+                else
+                {
+                    days.Enqueue((int)temp);
+                }
+            }
+
+            int cnt = 1;
+            int iter = days.Dequeue();
+
+            List<int> answerList = new List<int>();
+
+            while(days.Count != 0)
+            {
+                if(days.Peek() > iter)
+                {
+                    answerList.Add(cnt);
+                    cnt = 1;
+
+                    iter = days.Dequeue();
+
+                    if(days.Count == 0)
+                    {
+                        answerList.Add(1);
+                        break;
+                    }
+                }
+                else
+                {
+                    cnt++;
+                    days.Dequeue();
+
+                    if (days.Count == 0)
+                    {
+                        answerList.Add(cnt);
+                        break;
+                    }
+                }
+            }
+
+            for(int i=0; i<answerList.Count; i++)
+            {
+                answer[i] = answerList[i];
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            int[] progresses = { 95, 90, 99, 99, 80, 99 };
+            int[] speeds = { 1, 1, 1, 1, 1, 1 };
+
+            int[] ans = solution(progresses, speeds);
+
+            for(int i=0; i<ans.Length; i++)
+            {
+                Console.WriteLine(ans[i]);
+            }
+        }
+    }
+}
---
+++ 프로그래머스_Lv2/다리를 지나는 트럭.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 다리를_지나는_트럭
+    {
+        static int solution(int bridge_length, int weight, int[] truck_weights)
+        {
+            int answer = 0;
+
+            Queue<int> truck_On_Bridge = new Queue<int>();
+            Queue<int> time = new Queue<int>();
+
+            int cur = 1;
+            int end = 0;
+            int iter = 1;
+
+            truck_On_Bridge.Enqueue(truck_weights[0]);
+            time.Enqueue(1);
+            int curWeight = truck_weights[0];
+
+            while (end != truck_weights.Length)
+            {
+                cur++;
+
+                //맨앞의 트럭이 다리를 건너간 경우
+                if(cur - time.Peek() == bridge_length)
+                {
+                    curWeight -= truck_On_Bridge.Dequeue();
+                    time.Dequeue();
+                    end++;
+                }
+
+                //다음 트럭이 다리에 올라탈 수 있는 경우
+                if(iter < truck_weights.Length)
+                {
+                    if (truck_weights[iter] + curWeight <= weight)
+                    {
+                        curWeight += truck_weights[iter];
+                        truck_On_Bridge.Enqueue(truck_weights[iter]);
+                        time.Enqueue(cur);
+                        iter++;
+                    }
+                }
+            }
+
+            answer = cur;
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            int ans = solution(2, 10, new int[] { 7,4,5,6});
+            Console.WriteLine(ans);
+        }
+    }
+}
