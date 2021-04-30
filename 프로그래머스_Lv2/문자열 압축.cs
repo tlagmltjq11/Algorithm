da7e8d7cb0a19dbce54165594d32d8b31e889b04
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-04-30 19:39:34 +0900

    프로그래머스_Lv2

---
+++ 프로그래머스_Lv2/문자열 압축.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 문자열_압축
+    {
+        static int solution(string s)
+        {
+            int answer = 1001;
+
+            for(int i=1; i <= s.Length / 2; i++)
+            {
+                int cnt = 1;
+                string result = "";
+                string temp = s.Substring(0, i);
+
+                for(int j=i; j<=s.Length; j += i)
+                {
+                    if((j + i) > s.Length)
+                    {
+                        if(cnt == 1)
+                        {
+                            result += temp;
+                            result += s.Substring(j);
+                        }
+                        else
+                        {
+                            result += cnt.ToString() + temp;
+                            result += s.Substring(j);
+                        }
+
+                        break;
+                    }
+
+                    string iter = s.Substring(j, i);
+                    if (!temp.Equals(iter))
+                    {
+                        if(cnt == 1)
+                        {
+                            result += temp;
+                        }
+                        else
+                        {
+                            result += cnt.ToString() + temp;
+                        }
+
+                        temp = iter;
+                        cnt = 1;
+                    }
+                    else
+                    {
+                        cnt++;
+                    }
+                }
+                answer = Math.Min(answer, result.Length);
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution("xababcdcdababcdcd"));
+        }
+    }
+}
---
+++ 프로그래머스_Lv2/카카오프렌즈 컬러링북.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 카카오프렌즈_컬러링북
+    {
+        struct Point
+        {
+            public int x, y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        static int[] solution(int m, int n, int[,] picture)
+        {
+            int numberOfArea = 0;
+            int maxSizeOfOneArea = 0;
+
+            bool[,] visited = new bool[m, n];
+
+            Stack<Point> stack = new Stack<Point>();
+
+            for(int i=0; i<m; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if(picture[i, j] != 0 && visited[i, j] != true)
+                    {
+                        numberOfArea++;
+                        stack.Push(new Point(i, j));
+                        visited[i, j] = true;
+
+                        int cnt = 1;
+
+                        while(stack.Count != 0)
+                        {
+                            Point temp = stack.Pop();
+                            int x = temp.x;
+                            int y = temp.y;
+
+                            if(visited[x,y] == false)
+                            {
+                                visited[x, y] = true;
+                                cnt++;
+                            }
+
+                            if(x - 1 >= 0 && !visited[x - 1, y] && picture[x - 1, y] == picture[i, j])
+                            {
+                                stack.Push(new Point(x - 1, y));
+                            }
+
+                            if (x + 1 < m && !visited[x + 1, y] && picture[x + 1, y] == picture[i, j])
+                            {
+                                stack.Push(new Point(x + 1, y));
+                            }
+
+                            if (y - 1 >= 0 && !visited[x, y - 1] && picture[x, y - 1] == picture[i, j])
+                            {
+                                stack.Push(new Point(x, y - 1));
+                            }
+
+                            if (y + 1 < n && !visited[x, y + 1] && picture[x, y + 1] == picture[i, j])
+                            {
+                                stack.Push(new Point(x, y + 1));
+                            }
+                        }
+
+                        maxSizeOfOneArea = Math.Max(maxSizeOfOneArea, cnt);
+                    }
+                }
+            }
+
+            int[] answer = new int[2];
+            answer[0] = numberOfArea;
+            answer[1] = maxSizeOfOneArea;
+            return answer;
+        }
+
+        static void Main()
+        {
+            int[] answer = solution(6, 4, new int[,] { { 1, 1, 1, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 1 }, { 0, 0, 0, 1 }, { 0, 0, 0, 1 }, { 0, 0, 0, 1 } });
+
+            Console.WriteLine(answer[0].ToString());
+            Console.WriteLine(answer[1].ToString());
+        }
+    }
+}
