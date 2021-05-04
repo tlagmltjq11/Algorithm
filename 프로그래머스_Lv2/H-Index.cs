7b7d5e092de6adf875decd66cee8c4f6789e6193
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-05-04 22:52:40 +0900

    프로그래머스

---
+++ 프로그래머스_Lv2/H-Index.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class H_Index
+    {
+        static int solution(int[] citations)
+        {
+            int answer = 0;
+
+            Array.Sort(citations);
+
+            if(citations[citations.Length-1] == 0)
+            {
+                return 0;
+            }
+
+            for(int i=citations.Length-1; i>=0; i--)
+            {
+                if (citations[i] > answer)
+                {
+                    answer++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution(new int[] {3,0,6,1,5 }));
+        }
+    }
+}
---
+++ 프로그래머스_Lv2/위장.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 위장
+    {
+        static int solution(string[,] clothes)
+        {
+            int answer = 1;
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for(int i=0; i<clothes.GetLength(0); i++)
+            {
+                if(dict.ContainsKey(clothes[i, 1]))
+                {
+                    dict[clothes[i, 1]]++;
+                }
+                else
+                {
+                    dict.Add(clothes[i, 1], 1);
+                }
+            }
+
+            foreach(var cloth in dict)
+            {
+                answer *= (cloth.Value+1);
+            }
+
+            answer--;
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution(new string[,] { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" }, { "green_turban", "headgear" } }));
+        }
+    }
+}
