84468abc21c5573d745ea942ef7284c3a6b4d5e5
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-05-04 23:44:32 +0900

    프로그래머스

---
+++ 프로그래머스_Lv2/카펫.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 카펫
+    {
+        static int[] solution(int brown, int yellow)
+        {
+            int[] answer = new int[2];
+
+            int sum = brown + yellow; //전체 정사각형의 넓이
+
+            for(int i=3; ; i++)
+            {
+                int temp = 0;
+
+                if(sum % i == 0)
+                {
+                    temp = sum / i;
+
+                    if((i-2) * (temp - 2) == yellow)
+                    {
+                        answer[0] = temp;
+                        answer[1] = i;
+                        break;
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+
+        }
+    }
+}
