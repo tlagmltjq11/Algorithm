511d1641fa812df141cc93f593c405bc9bdcf2c1
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-05-13 15:12:11 +0900

    프로그래머스

---
+++ 프로그래머스_Lv2/멀쩡한 사각형.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 멀쩡한_사각형
+    {
+        static long gcd(long a, long b)
+        {
+            long temp;
+            while(b > 0)
+            {
+                temp = a % b;
+                a = b;
+                b = temp;
+            }
+
+            return a;
+        }
+
+        static long solution(int w, int h)
+        {
+            long answer = 1;
+
+            long gcdResult = gcd(w, h);
+
+            answer = (long)w * (long)h - ((long)w + (long)h - gcdResult);
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.Write(solution(8, 12));
+        }
+    }
+}
