b537cf6a0e99a55b4555cc7924ad0db2786a8b7e
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-05-17 01:57:17 +0900

    프로그래머스

---
+++ 프로그래머스_Lv2/예상 대진표.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 예상_대진표
+    {
+        static int solution(int n, int a, int b)
+        {
+            int answer = 1;
+
+            int min, max;
+
+            if(a > b)
+            {
+                min = b;
+                max = a;
+            }
+            else
+            {
+                min = a;
+                max = b;
+            }
+
+            while(true)
+            {
+                if(min + 1 == max && max % 2 == 0)
+                {
+                    break;
+                }
+
+                min = (min + 1) / 2;
+                max = (max + 1) / 2;
+
+                answer++;
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution(8, 4, 7));
+        }
+    }
+}
