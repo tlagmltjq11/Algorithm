689f7d7c28381847b07bbbec4e9b6f0c2c9e5432
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-05-28 15:49:36 +0900

    프로그래머스

---
+++ 프로그래머스_Lv2/괄호 회전하기.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 괄호_회전하기
+    {
+        static bool Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for(int i=0; i<str.Length; i++)
+            {
+                if (str[i].Equals('(') || str[i].Equals('{') || str[i].Equals('['))
+                {
+                    stack.Push(str[i]);
+                }
+                else
+                {
+                    if(stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char temp = stack.Pop();
+
+                    if(str[i].Equals(')'))
+                    {
+                        if(!temp.Equals('('))
+                        {
+                            return false;
+                        }
+                    }
+                    else if(str[i].Equals('}'))
+                    {
+                        if (!temp.Equals('{'))
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (!temp.Equals('['))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            if(stack.Count != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static int solution(string s)
+        {
+            int answer = 0;
+
+            //회전
+            for(int i=0; i<s.Length; i++)
+            {
+                char first = s[0];
+                s = s.Substring(1) + first;
+
+                if(Check(s))
+                {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution("({)}"));
+        }
+    }
+}
---
+++ 프로그래머스_Lv2/후보키.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 후보키
+    {
+        static int cnt = 0;
+        static string[,] table;
+
+        static void Combination(int depth, List<int> keys)
+        {
+            if(depth == table.GetLength(1))
+            {
+                return;
+            }
+
+            for(int i=depth; i<table.GetLength(1); i++)
+            {
+                keys.Add(i);
+                if(IsKey(keys))
+                {
+                    cnt++;
+                    keys.Remove(i);
+                    continue;
+                }
+
+                Combination(i + 1, keys);
+                keys.Remove(i);
+            }
+        }
+
+        static bool IsKey(List<int> keys)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            for(int i=0; i<table.GetLength(0); i++)
+            {
+                string temp = string.Empty;
+
+                for(int j=0; j<keys.Count; j++)
+                {
+                    temp += table[i, keys[j]];
+                }
+
+                if(set.Contains(temp))
+                {
+                    return false;
+                }
+
+                set.Add(temp);
+            }
+
+            return true;
+        }
+
+        static int solution(string[,] relation)
+        {
+            int answer = 0;
+            table = relation;
+
+            Combination(0, new List<int>());
+
+            answer = cnt;
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution(new string[,] {{"100", "r" },
+{ "200", "c" },
+{ "300", "d" } }));
+        }
+    }
+}
