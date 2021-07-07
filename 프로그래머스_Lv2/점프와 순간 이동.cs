767d76522f47a512b471d2ba3f0b3e43c13a528c
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-07-07 11:46:23 +0900

    프로그래머스_Lv2

---
+++ 프로그래머스_Lv2/점프와 순간 이동.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 점프와_순간_이동
+    {
+        static int solution(int n)
+        {
+            int answer = 0;
+
+            while(n > 0)
+            {
+                //나머지가 존재한다면 점프가 + 1 필요한 순간
+                if(n % 2 != 0)
+                {
+                    answer++;
+                }
+
+                n /= 2;
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution(5000));
+        }
+    }
+}
---
+++ 프로그래머스_Lv2/쿼드압축 후 개수 세기.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 쿼드압축_후_개수_세기
+    {
+        static int[] ans = new int[2];
+        static int[,] matrix;
+
+        static void Divide(int start_X, int start_Y, int size)
+        {
+            bool check = false;
+
+            if(size == 1)
+            {
+                ans[matrix[start_X, start_Y]]++;
+                return;
+            }
+
+            for(int i=start_X; i<start_X + size; i++)
+            {
+                for(int j=start_Y; j<start_Y + size; j++)
+                {
+                    if(matrix[i, j] != matrix[start_X, start_Y])
+                    {
+                        check = true;
+                        break;
+                    }
+                }
+            }
+
+            //start부터 end까지 모든 수가 동일한 경우
+            if(!check)
+            {
+                ans[matrix[start_X, start_Y]]++;
+                return;
+            }
+            //동일하지 않은 경우
+            else
+            {
+                size /= 2;
+                Divide(start_X, start_Y, size);
+                Divide(start_X, start_Y + size, size);
+                Divide(start_X + size, start_Y, size);
+                Divide(start_X + size, start_Y + size, size);
+            }
+
+            return;
+        }
+
+        static int[] solution(int[,] arr)
+        {
+            int[] answer = new int[2];
+            int size = arr.GetLength(0);
+
+            matrix = arr;
+            Divide(0, 0, size);
+
+            answer[0] = ans[0];
+            answer[1] = ans[1];
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            int[] result = solution(new int[,] { { 1, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 1 }, { 1, 1, 1, 1 } });
+
+            for(int i=0; i<result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+    }
+}
