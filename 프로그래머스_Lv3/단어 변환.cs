b9f5f634fced32d03e13c3ab9f5160c18a6a585a
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-07-20 20:47:17 +0900

    프로그래머스 Lv3

---
+++ 프로그래머스_Lv3/단어 변환.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 단어_변환
+    {
+        static bool[] visited;
+
+        static bool CanTranslate(string from, string to)
+        {
+            int cnt = 0;
+
+            for(int i=0; i<from.Length; i++)
+            {
+                if(!from[i].Equals(to[i]))
+                {
+                    cnt++;
+
+                    if(cnt > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static int solution(string begin, string target, string[] words)
+        {
+            int answer = 0;
+            int size = words.Length;
+            visited = new bool[size];
+
+            Queue<KeyValuePair<string, int>> q = new Queue<KeyValuePair<string, int>>();
+
+            q.Enqueue(new KeyValuePair<string, int>(begin, 0));
+
+            while(q.Count != 0)
+            {
+                KeyValuePair<string, int> temp = q.Dequeue();
+
+                if(temp.Key.Equals(target))
+                {
+                    answer = temp.Value;
+                    return answer;
+                }
+
+                for(int i=0; i<size; i++)
+                {
+                    if(visited[i] != true && CanTranslate(temp.Key, words[i]))
+                    {
+                        visited[i] = true;
+                        q.Enqueue(new KeyValuePair<string, int>(words[i], temp.Value + 1));
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution("hit", "cog", new string[] { "hot", "dot", "dog", "lot", "log", "cog"}));
+        }
+    }
+}
