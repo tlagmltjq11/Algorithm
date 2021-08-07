1b7e9f033d69884493285211eed6d7e931b8ac9d
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-08-07 19:34:58 +0900

    프로그래머스 Lv3

---
+++ 프로그래머스_Lv3/가장 긴 팰린드롬.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 가장_긴_팰린드롬
+    {
+        static bool isPalinDrome(string str, int start, int end)
+        {
+            int size = start + (end - start + 1) / 2; 
+
+            for(int i=start; i< size; i++)
+            {
+                if(!str[i].Equals(str[end]))
+                {
+                    return false;
+                }
+                end--;
+            }
+
+            return true;
+        }
+
+        static int solution(string s)
+        {
+            int answer = 0;
+
+            for(int i=s.Length; i>=1; i--)
+            {
+                for(int j=0; j<=s.Length - i; j++)
+                {
+                    if (isPalinDrome(s, j, j + i - 1))
+                    {
+                        answer = i;
+                        return answer;
+                    }
+                }
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution("abcabcdcbae"));
+        }
+    }
+}
