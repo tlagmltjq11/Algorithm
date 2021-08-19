0d7b3057db282a674d1d1a0347fc2b78dd685f36
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-08-19 17:16:51 +0900

    프로그래머스 Lv3

---
+++ 프로그래머스_Lv3/숫자 게임.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 숫자_게임
+    {
+        static int solution(int[] A, int[] B)
+        {
+            int answer = 0;
+
+            Array.Sort(A);
+            Array.Sort(B);
+
+            int iterA = A.Length -1;
+            int iterB = B.Length -1;
+
+            while(iterA >= 0)
+            {
+                if(B[iterB] > A[iterA])
+                {
+                    answer++;
+                    iterB--;
+                }
+                iterA--;
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.Write(solution(new int[] {5,1,3,7 }, new int[] {2,2,6,8 }));
+        }
+    }
+}
