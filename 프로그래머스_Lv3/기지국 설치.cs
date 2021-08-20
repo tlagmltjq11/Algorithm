badf370e0f3d04ff545e05c0b0b68b5e94453209
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-08-21 00:00:10 +0900

    프로그래머스  Lv3

---
+++ 프로그래머스_Lv3/기지국 설치.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class 기지국_설치
+    {
+        static int solution(int n, int[] stations, int w)
+        {
+            int answer = 0;
+            int prev = 1;
+            int next;
+            int counter = 2 * w + 1;
+            
+            for(int i=0; i<stations.Length; i++)
+            {
+                next = stations[i] - w;
+
+                int cur = next - prev;
+                if(cur > 0)
+                {
+                    if(cur % counter == 0)
+                    {
+                        answer += cur / counter;
+                    }
+                    else
+                    {
+                        answer += (cur / counter) + 1;
+                    }
+                }
+
+                prev = stations[i] + w + 1;
+            }
+
+            if (prev < n)
+            {
+                int cur = n - prev;
+
+                if (cur % counter == 0)
+                {
+                    answer += cur / counter;
+                }
+                else
+                {
+                    answer += (cur / counter) + 1;
+                }
+            }
+
+            return answer;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution(11, new int[] { 4, 11 }, 1));
+        }
+    }
+}
