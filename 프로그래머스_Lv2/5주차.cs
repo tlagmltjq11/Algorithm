94ee442a454d25daa0da31d2f5b16b6b283ba0e7
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-09-05 20:08:51 +0900

    프로그래머스 Lv2

---
+++ 프로그래머스_Lv2/5주차.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _5주차
+    {
+        static int solution(string word)
+        {
+            string str = "AEIOU";
+            int[] nums = new int[] { 781, 156, 31, 6, 1 };
+            int ret = 0;
+            for (int i = 0; i < word.Length; i++)
+                ret += str.IndexOf(word[i]) * nums[i] + 1;
+            return ret;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(solution("AAAAE"));
+        }
+    }
+}
