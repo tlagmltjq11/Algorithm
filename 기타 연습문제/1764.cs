51ab6846d23e2b0a53185a94d795348fe87d6765
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-09-27 17:15:31 +0900

    백준

---
+++ 기타 연습문제/1764.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _1764
+    {
+        static void Main()
+        {
+            string[] nm = Console.ReadLine().Split(" ");
+            int n = int.Parse(nm[0]);
+            int m = int.Parse(nm[1]);
+
+            HashSet<string> set = new HashSet<string>();
+            for(int i=0; i<n; i++)
+            {
+                string temp = Console.ReadLine();
+                set.Add(temp);
+            }
+
+            List<string> answer = new List<string>();
+            for(int i=0; i<m; i++)
+            {
+                string temp = Console.ReadLine();
+
+                if(set.Contains(temp))
+                {
+                    answer.Add(temp);
+                }
+            }
+
+            Console.WriteLine(answer.Count);
+            answer.Sort();
+            for(int i=0; i<answer.Count; i++)
+            {
+                Console.WriteLine(answer[i]);
+            }
+        }
+    }
+}
---
+++ 기타 연습문제/5525.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _5525
+    {
+        static void Main()
+        {
+            int answer = 0;
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder("I");
+            for (int i=0; i<n; i++)
+            {
+                sb.Append("OI");
+            }
+
+            string compare = sb.ToString();
+            int size = compare.Length;
+            for (int i=0; i<=m-size; i++)
+            {
+                if(s.Substring(i, size).Equals(compare))
+                {
+                    answer++;
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
