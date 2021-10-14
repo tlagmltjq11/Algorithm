15cfee93f3b4c95a126b8701a7bab91cde47ee2d
tlagmltjq11 <43705434+tlagmltjq11@users.noreply.github.com>
2021-10-14 16:43:35 +0900

    DP

---
+++ DP/11726.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _11726
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int[] dp = new int[1001];
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 3;
+
+            for(int i=4; i<=num; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+                dp[i] %= 10007;
+            }
+
+            Console.WriteLine(dp[num]);
+        }
+    }
+}
---
+++ DP/9095.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingTestPractice
+{
+    class _9095
+    {
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine());
+            int[] dp = new int[11];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for(int i=4; i<=10; i++)
+            {
+                dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<t; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sb.AppendLine(dp[num].ToString());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
